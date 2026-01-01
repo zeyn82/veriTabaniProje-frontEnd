0c2cdba75e40067391e9a32b18cc3033968f84fa
Melike Rana Sünger <melike.rana.sunger59@erzurum.edu.tr>
2026-01-01 16:49:09 +0300

    index.js test endpoint güncellendi.

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// 🔹 TEST ENDPOINT
+app.get('/api/test', (req, res) => {
+  res.json({ message: "Backend çalışıyor 🚀" });
+});
+
+// 🔹 ROUTES
 app.use('/api', yolcuRoutes);
 app.use('/api', ucusRoutes);
 app.use('/api', biletRoutes);
